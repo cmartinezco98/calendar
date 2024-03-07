a8dd29590839a68efce3695c55f5657d2cf31397
Sebastian <sebastianjimenez0397@gmail.com>
2024-03-06 19:10:00 -0500

    Se borra una s

--- src/modules/mailer/mail.controller.ts
+++ src/modules/mailer/mail.controller.ts
@@ -1,10 +1,10 @@
 import { Body, Controller, Get } from '@nestjs/common';
-import { MailService } from './mail.service';
 import { CreateMailerDto } from './dto/create-mailer.dto';
+import { MailService } from './mail.service';
 
 @Controller('mailer')
 export class MailController {
-  constructor(private readonly mailService: MailService) { } s
+  constructor(private readonly mailService: MailService) { }
   @Get()
   sendMail(@Body() dataEmail: CreateMailerDto) {
     return this.mailService.sendMail(dataEmail);
